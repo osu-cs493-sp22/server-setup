6ae458c1e683d8c198b092ff999d6d620e2e1964
Rob Hess <hessro@oregonstate.edu>
2022-04-06 15:24:40 -0700

    Add code from 4/6 lecture.

---
+++ hello.js
@@ -0,0 +1,24 @@
+const circle = require('./circle');
+const fs = require('fs');
+const figlet = require('figlet');
+
+console.log("Hello World!");
+
+console.log("== process.env:", process.env);
+console.log("== process.env.MY_ENV_VARIABLE:", process.env.MY_ENV_VARIABLE);
+
+console.log("== circle:", circle);
+console.log("== circle.circumference(5):", circle.circumference(5));
+console.log("== circle.area(5):", circle.area(5));
+
+fs.readFile('circle.js', 'utf-8', function (err, data) {
+    if (!err) {
+        console.log(data);
+    }
+});
+
+figlet('Hello, CS 493!', function (err, data) {
+    if (!err) {
+        console.log(data);
+    }
+})
